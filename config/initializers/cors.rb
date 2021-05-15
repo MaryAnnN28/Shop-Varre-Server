abaaafe04781191539656181410deb6f87711427
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-05-15 11:49:46 -0400

    added server side/heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,13 +8,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
   
-  allow do
-    origins "https://varre-shop-app.herokuapp.com"
-    # This is where the domain of where you are hosting to 
+  # allow do
+  #   origins "https://varre-shop-app.herokuapp.com"
+  #   # This is where the domain of where you are hosting to 
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head], 
-      credentials: true
-  end
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+  #     credentials: true
+  # end
 end
